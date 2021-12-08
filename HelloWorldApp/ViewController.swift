0789096c3374b16d5d863bc3793e2b4b6867a2ea
Elena <ivanovelens@gmail.com>
2021-12-08 17:53:00 +0300

    Work with logic

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
+        helloWorldLabel.textColor = .systemRed
     }
 
-
+    @IBAction func startPressed() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden{
+            startButton.setTitle("Show test", for: .normal)
+        } else {
+            startButton.setTitle("Hide text", for: .normal)
+        }
+    }
+    
 }
 
